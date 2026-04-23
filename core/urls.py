c15df5a4db37061ad00e193320540b7a856b5ef0
phd-benel <phd-benel@gmail.com>
2026-04-23 17:01:14 +0100

    Add Sentry Bug

--- core/urls.py
+++ core/urls.py
@@ -20,5 +20,6 @@ from portal.views import home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home, name='home'), # La racine du site
+    path('', home, name='home'),
+    path('sentry-debug/', trigger_error, name='sentry_debug'), 
 ]
