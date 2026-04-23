f6f0a3f86cdb8412d60c98fa103365d379fd2049
phd-benel <phd-benel@gmail.com>
2026-04-23 17:03:17 +0100

    Add Sentry Bug

--- core/urls.py
+++ core/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from portal.views import home
+from portal.views import home , trigger_error
 
 urlpatterns = [
     path('admin/', admin.site.urls),
